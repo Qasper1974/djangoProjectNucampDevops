68cf88a88862be81835ab9b93954f3fd972346bd
Yves Faems <yves.faems@gmail.com>
2022-01-21 19:48:45 -0800

    register Room model

--- base/views.py
+++ base/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render
 
+from .models import Room
+
 # Create your views here.
 
-rooms =[
-{ 'id': 1, 'name' : 'Lets learn Python'},
-{ 'id': 2, 'name' : 'Design with me'},
-{ 'id': 3, 'name' : 'Frontend Developers'},
-
-]
+# rooms =[
+# { 'id': 1, 'name' : 'Lets learn Python'},
+# { 'id': 2, 'name' : 'Design with me'},
+# { 'id': 3, 'name' : 'Frontend Developers'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'kamers': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    kamer = None
-    
-    for i in rooms:
-        if i['id'] == int(pk):
-            kamer = i
+    kamer = Room.objects.get(id=pk)
     context = {'room': kamer}
     return render(request, 'base/room.html', context)
